5b044ccea63dace743ef6c2b14999bd9381a9345
tauri_s <tauristclaire@gmail.com>
2022-09-08 17:37:55 -0700

    Function to count how many wrong guesses left, to display to user, and async to call in random word list and word but right now calls alllll the words

--- js/script.js
+++ js/script.js
@@ -10,15 +10,38 @@ const wordInProgress = document.querySelector(".word-in-progress");
 //The paragraph where the remaining guesses will display.
 const guessesLeftParagraph = document.querySelector(".remaining");
 //The span inside the paragraph where the remaining guesses will display.
-const guessedLeftSpan = document.querySelector(".remaining span");
+const guessesLeftSpan = document.querySelector(".remaining span");
 //The empty paragraph where messages will appear when the player guesses a letter.
 const guessMessage = document.querySelector(".message");
 //The hidden button that will appear prompting the player to play again
 const playAgainButton = document.querySelector(".play-again");
 //Start word before APR
-const word = "magnolia";
+let word = "magnolia";
 //Empty array for all guessed letters
 const guessedLetters = [];
+//Remaining wrong guesses left
+let remainingGuesses = 8;
+
+//Async for word txt
+const getWord = async function () {
+    const request = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const words = await request.text();
+    //console.log(words);
+    //Grab a random word from the array
+        //Split words into an array with split()
+        //Use line breaks to seperate them with a delimiter n
+    const wordArray = words.split("/n");
+    console.log(wordArray);
+        //Pull random word by produce a random number and multiply by how many items in array
+        //Round the number down 0-99 like array index
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
+        //Trim() white space in text file
+        word = wordArray[randomIndex].trim();
+        hiddenLetters(word);
+};
+
+//Call Async function
+getWord();
 
 //Hide the start word with circles
 const hiddenLetters = function (word) {
@@ -77,6 +100,7 @@ const makeGuess = function (guess) {
     else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        countGuesses(guess);
         displayGuessedLetters();
         updateWordInProgress(guessedLetters);
     }
@@ -110,6 +134,26 @@ const updateWordInProgress = function (guessedLetters) {
     //Update the empty paragraph where the word in progress appears
     wordInProgress.innerText = updatedLetters.join("");
     didYouWin();
+};
+
+//Count players guesses
+const countGuesses = function (guess) {
+    const upperWord = word.toUpperCase();
+    if (!upperWord.includes(guess)) {
+        guessMessage.innerText = `Sorry, the word has no ${guess}.`;
+        remainingGuesses -= 1;
+    } else {
+        guessMessage.innerText = `Yes! The word does have the letter ${guess}!`;
+    }
+//Display how many guesses left
+    //If no more guesses, display message
+    if (remainingGuesses === 0) {
+        guessMessage.innerText = `You ran out of guesses! The word was <span class="highlight">${word}</span>.`;
+    } else if (remainingGuesses === 1) {
+        guessesLeftSpan.innerText = `You only have one more guess remaining!`;
+    } else {
+        guessesLeftSpan.innerText = `You have ${remainingGuesses} remaining!`
+    }
 };
 
 //Check if player wins
